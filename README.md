1a1373bb6f5115337b5306c4de8ed271a9680823
dokelung <dokelung@gmail.com>
2017-11-08 12:16:57 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,188 @@
 # ptt.py
+
+## QuickStart
+
+```python
+from ptt import ArticleListPage
+
+page = ArticleListPage.from_board('gossiping')
+
+for s in page:
+    if s.isremoved:
+        continue
+    a = s.read()
+    print(a.dump_json())
+```
+
+-----------------------------------------------------------
+
+## class ArticleSummary: alias to Summary
+
+### attr
+
+(string) ArticleSummary.title          title of Article
+(string) ArticleSummary.category       string in syntax "[" and "]" of title
+
+(string) ArticleSummary.url            url of the Article without ptt domain name
+(string) ArticleSummary.board          board name of Article
+(string) ArticleSummary.aid            Article ID
+
+(string) ArticleSummary.date           string of Article date (e.g. '7/24')
+(string) ArticleSummary.author         string of Article author (only author id)
+
+(string) ArticleSummary.score          string of score or '爆' for score>99 or 'X' for score<0
+(string) ArticleSummary.mark           Article mark (e.g. M)
+
+(string) ArticleSummary.removeinfo     remove infomation written in title
+
+( bool ) ArticleSummary.isreply        True if 'Re:' in title else False
+( bool ) ArticleSummary.isforward      True if 'Fw:' in title else False
+( bool ) ArticleSummary.isremoved      True if Article has been removed else False
+
+### API
+
+(ArticlePage) ArticleSummary.read() 
+    return corresponding ArticlePage if it is not removed
+    
+### classmethod
+
+(ArticleSummary) ArticleSummary.from_bs_tag(tag)
+    return ArticleSummary by summary tag
+
+-----------------------------------------------------------
+
+## class Page [DO NOT USE IT DIRECTLY]
+
+### class attr
+
+(string) Page.ptt_domain                'https://www.ptt.cc'
+(string) Page.ask_over_18_url           '/ask/over18'
+
+### attr
+ 
+(string) Page.url                       Page url without ptt domain name
+(string) Page.html                      html of Page
+
+-----------------------------------------------------------
+
+## class ArticleListPage(Page)
+
+### attr
+
+(string) ArticleListPage.board                  board name of this ArticleListPage
+( int  ) ArticleListPage.idx                    index of ArticleListPage
+
+( list ) ArticleListPage.article_summary_tags   bs tags of ArticleSummary
+
+( dict ) ArticleListPage.related_urls           urls of related pages        
+            ArticleListPage.related_urls['board']
+            ArticleListPage.related_urls['man']
+            ArticleListPage.related_urls['previous']
+            ArticleListPage.related_urls['next']
+            ArticleListPage.related_urls['oldest']
+            ArticleListPage.related_urls['newest']
+
+( generator ) ArticleListPage.article_summaries list of ArticleSummary of this ArticleListPage
+
+(ArticleListPage) ArticleListPage.previous ArticleListPage of related_urls['previous']
+(ArticleListPage) ArticleListPage.next     ArticleListPage of related_urls['next']
+(ArticleListPage) ArticleListPage.oldest   ArticleListPage of related_urls['oldest']
+(ArticleListPage) ArticleListPage.newest   ArticleListPage of related_urls['newest']
+
+### classmethod ----------------------------------------------------------------------------------
+
+@alias to Board(board, index)
+(ArticleListPage) ArticleListPage.from_board(board, index)
+    return ArticleListPage by board name (and index)
+
+### iter -----------------------------------------------------------------------------------------
+    
+```python
+for ArticleSummary in ArticleListPage:
+    do something with ArticleSummary
+```
+
+-----------------------------------------------------------
+
+## class ArticlePage(Page): alias to Article
+
+### attr
+
+(string)   ArticlePage.title       title of Article
+(string)   ArticlePage.category    string in "[" and "]" of title
+
+(string)   ArticlePage.board       board name of Article
+(string)   ArticlePage.aid         Article ID
+
+(string)   ArticlePage.date        string of Article date (e.g. Tue Feb 16 20:15:23 2016)
+(datetime) ArticlePage.datetime    datetime format of date
+
+(string)   ArticlePage.author      string of Article author
+(string)   ArticlePage.ip          author's ip
+(stirng)   ArticlePage.signature   signature string of the author
+
+(Pushes)   ArticlePage.pushes      Pushes is a class which collects all pushes in article
+(string)   ArticlePage.content     main content of article using html format
+
+( bool )   ArticlePage.isreply     True if 'Re:' in title else False
+( bool )   ArticlePage.isforward   True if 'Fw:' in title else False
+
+### API
+
+(string)  ArticlePage.dump_json()
+    dump json string with data
+        "board", "aid", "author", "date", "content", "ip", "pushes_count", "pushes"
+
+### classmethod
+
+(ArticlePage) ArticlePage.from_board_aid(board, aid)
+    return  ArticlePage by board name and aid
+
+-----------------------------------------------------------
+
+## class Pushes
+
+### attr
+
+(ArticlePage) Pushes.article            ArticlePage of these pushes
+
+( list )      Pushes.msgs               list of Msg(self defined namedtuple)
+
+( dict )      Pushes.count              all types of count
+                Pushes.count['all']       total msg in Pushes
+                Pushes.count['score']     positive msg count - negative msg count
+                Pushes.count['like']      positive msg count
+                Pushes.count['boo']       negative msg count
+                Pushes.count['neutral']   neutral msg count
+
+(string)      Pushes.simple_expression  string expression of all msgs
+            
+### API
+
+(void)        Pushes.addmsg(Msg)        add a msg to Pushes
+(void)        Pushes.countit            set Pushes.count
+
+-----------------------------------------------------------
+
+## nameedtuple Msg
+
+```python
+collections.namedtuple('Msg', ['type', 'user', 'content', 'ipdatetime'])
+```
+
+-----------------------------------------------------------
+
+## self defined Exceptions
+
+* InValidBeautifulSoupTag
+* NoGivenURLForPage
+* PageNotFound
+* ArtitcleIsRemove
+
+-----------------------------------------------------------
+
+## utility functions
+
+* parse_std_url
+* parse_title
+* parse_username
---
+++ ptt.py
@@ -0,0 +1,340 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib
+import datetime
+import collections
+import json
+
+
+# exception
+class InValidBeautifulSoupTag(Exception): pass
+class NoGivenURLForPage(Exception): pass
+class PageNotFound(Exception): pass
+class ArtitcleIsRemove(Exception): pass
+
+
+# utility
+def parse_std_url(url):
+    prefix, _,  basename = url.rpartition('/')
+    basename, _, _ = basename.rpartition('.')
+    bbs, _, board = prefix.rpartition('/')
+    bbs = bbs[1:]
+    return bbs, board, basename
+
+def parse_title(title):
+    _, _, remain = title.partition('[')
+    category, _, remain = remain.rpartition(']')
+    category = category if category else None
+    isreply = True if 'Re:' in title else False
+    isforward = True if 'Fw:' in title else False
+    return category, isreply, isforward
+
+def parse_username(full_name):
+    name, nickname = full_name.split(' (')
+    nickname = nickname.rstrip(')')
+    return name, nickname
+
+Msg = collections.namedtuple('Msg', ['type', 'user', 'content', 'ipdatetime'])
+
+
+class ArticleSummary(object):
+
+    def __init__(self, title, url, score, date, author, mark, removeinfo):
+
+        # title
+        self.title = title
+        self.category, self.isreply, self.isforward = parse_title(title)
+
+        # url
+        self.url = url
+        _, self.board, self.aid = parse_std_url(url)
+
+        # meta
+        self.score = score
+        self.date = date
+        self.author = author
+        self.mark = mark
+
+        # remove
+        self.isremoved = True if removeinfo else False
+        self.removeinfo = removeinfo
+
+    @classmethod
+    def from_bs_tag(cls, tag):
+
+        try:
+            removeinfo = None
+            title_tag = tag.find('div', class_='title')
+            a_tag = title_tag.find('a')
+
+            if not a_tag:
+                removeinfo = title_tag.get_text().strip()
+
+            if not removeinfo: 
+                title = a_tag.get_text().strip()
+                url = a_tag.get('href').strip()
+                score = tag.find('div', class_='nrec').get_text().strip()
+            else:
+                title = '本文章已被刪除'
+                url  = ''
+                score = ''
+
+            date = tag.find('div', class_='date').get_text().strip()
+            author = tag.find('div', class_='author').get_text().strip()
+            mark = tag.find('div', class_='mark').get_text().strip()
+        except:
+            print(tag)
+            raise InValidBeautifulSoupTag
+
+        return cls(title, url, score, date, author, mark, removeinfo)
+
+    def __repr__(self):
+        return '<Summary of Article("{}")>'.format(self.url)
+
+    def __str__(self):
+        return self.title
+
+    def read(self):
+        if self.isremoved:
+            raise ArtitcleIsRemove
+        return ArticlePage(self.url)
+
+
+class Page(object):
+    
+    ptt_domain = 'https://www.ptt.cc'
+    ask_over_18_url = '/ask/over18'
+
+    def __init__(self, url):
+        if not url:
+            raise NoGivenURLForPage
+
+        self.url = url
+
+        s = requests.Session()
+        url = urllib.parse.urljoin(self.ptt_domain, self.url)
+
+        # to pass over18 check
+        aurl = urllib.parse.urljoin(self.ptt_domain, self.ask_over_18_url)
+        s.post(aurl, data={'yes': 'yes'})
+
+        # to fetch page
+        resp = s.get(url)
+        if resp.status_code == requests.codes.ok:
+            self.html = resp.text
+        else:
+            print(resp.status_code)
+            raise PageNotFound
+
+
+class ArticleListPage(Page):
+
+    def __init__(self, url):
+        super().__init__(url)
+
+        # to set article_tags
+        soup = BeautifulSoup(self.html, 'lxml')
+        self.article_summary_tags = soup.find_all('div', 'r-ent')
+        self.article_summary_tags.reverse()
+
+        # to set related urls
+        action_tags = soup.find('div', class_='action-bar').find_all('a')
+        self.related_urls = {}
+        url_names = 'board man oldest previous next newest'
+        for idx, name in enumerate(url_names.split()):
+            self.related_urls[name] = action_tags[idx].get('href')
+
+        # to set board and idx
+        _, self.board, basename = parse_std_url(url)
+        _, _, idx = basename.partition('index')
+        if idx:
+            self.idx = int(idx)
+        else:
+            _, self.board, basename = parse_std_url(self.related_urls['previous'])
+            _, _, idx = basename.partition('index')
+            self.idx = int(idx)+1
+
+    @classmethod
+    def from_board(cls, board, index=''):
+        url = '/'.join(['/bbs', board, 'index'+str(index)+'.html'])
+        return cls(url)
+
+    def __repr__(self):
+        return 'ArticleListPage("{}")'.format(self.url)
+
+    def __iter__(self):
+        return self.article_summaries
+
+    @property
+    def article_summaries(self):
+        return (ArticleSummary.from_bs_tag(tag) for tag in self.article_summary_tags)
+
+    @property
+    def previous(self):
+        return ArticleListPage(self.related_urls['previous'])
+
+    @property
+    def next(self):
+        return ArticleListPage(self.related_urls['next'])
+
+    @property
+    def oldest(self):
+        return ArticleListPage(self.related_urls['oldest'])
+
+    @property
+    def newest(self):
+        return ArticleListPage(self.related_urls['newest'])
+
+
+class ArticlePage(Page):
+
+    def __init__(self, url):
+        super().__init__(url)
+
+        _, _, self.aid = parse_std_url(url)
+
+        # to set article_tags
+        soup = BeautifulSoup(self.html, 'lxml')
+        main_tag = soup.find('div', id='main-content')
+        meta_name_tags = main_tag.find_all('span', class_='article-meta-tag')
+        meta_value_tags = main_tag.find_all('span', class_='article-meta-value')
+
+        # dealing meta
+        self.author = meta_value_tags[0].get_text().strip()
+        self.board = meta_value_tags[1].get_text().strip()
+        self.title = meta_value_tags[2].get_text().strip()
+        self.date = meta_value_tags[3].get_text().strip()
+
+        self.category, self.isreply, self.isforward = parse_title(self.title)
+        self.datetime = datetime.datetime.strptime(self.date, '%a %b %d %H:%M:%S %Y')
+
+        # remove meta
+        for tag in main_tag.select('div.article-metaline'):
+            tag.extract()
+        for tag in main_tag.select('div.article-metaline-right'):
+            tag.extract()
+
+        # fetch pushes and remove them
+        self.pushes = Pushes(self)
+        push_tags = main_tag.find_all('div', class_='push')
+        for tag in push_tags:
+            tag.extract()
+        for tag in push_tags:
+            if not tag.find('span', 'push-tag'):
+                continue
+            push_type = tag.find('span', class_='push-tag').string.strip(' \t\n\r')
+            push_user = tag.find('span', class_='push-userid').string.strip(' \t\n\r')
+            push_content = tag.find('span', class_='push-content').strings
+            push_content = ' '.join(push_content)[1:].strip(' \t\n\r')
+            push_ipdatetime = tag.find('span', class_='push-ipdatetime').string.strip(' \t\n\r')
+            msg = Msg(type=push_type, user=push_user, content=push_content, ipdatetime=push_ipdatetime)
+            self.pushes.addmsg(msg)
+        self.pushes.countit()
+
+        # handle special item
+        ip_tags = main_tag.find_all('span', class_='f2')
+        dic = {}
+        for tag in ip_tags:
+            if '※' in tag.get_text():
+                key, _, value = tag.get_text().partition(':')
+                key = key.strip('※').strip()
+                value = value.strip()
+                if '引述' in key:
+                    continue
+                else:
+                    dic.setdefault(key, []).append(value)
+                    tag.extract()
+        self.ip = dic['發信站'][0].split()[-1]
+
+        # remove richcontent
+        for tag in main_tag.find_all('div', class_='richcontent'):
+            tag.extract()
+
+        # handle trans
+        trans = []
+        for tag in main_tag.find_all('span', class_='f2'):
+            if '轉錄至看板' in tag.get_text():
+                trans.append(tag.previous_element.parent)
+                trans.append(tag.get_text())
+                trans.append(tag.next_sibling)
+                tag.previous_element.parent.extract()
+                tag.next_sibling.extract()
+                tag.extract()
+
+        # split main content and signature
+        self.content, self.signature = str(main_tag).split('--')[:2]
+        self.content = self.content.strip()
+
+        contents = self.content.split('\n')
+        self.content = '\n'.join(content for content in contents if not ('<div' in content and 'main-content' in content))
+
+        contents = self.signature.split('\n')
+        self.signature = '\n'.join(content for content in contents if not ('</div' in content))
+
+    @classmethod
+    def from_board_aid(cls, board, aid):
+        url = '/'.join(['/bbs', board, aid+'.html'])
+        return cls(url)
+
+    def __repr__(self):
+        return 'ArticlePage("{}")'.format(self.url)
+
+    def __str__(self):
+        return self.title
+
+    def dump_json(self):
+        data = {
+            'board': self.board,
+            'aid': self.aid,
+            'author': self.author,
+            'date': self.date,
+            'content': self.content,
+            'ip': self.ip,
+            'pushes_count': self.pushes.count,
+            'pushes': self.pushes.simple_expression
+        }
+        return json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False)
+
+
+class Pushes:
+
+    def __init__(self, article):
+        self.article = article
+        self.msgs = []
+
+    def __repr__(self):
+        return 'Pushes({})'.format(repr(self.article))
+
+    def __str__(self):
+        return 'Pushes of Article {}'.format(self.Article)
+
+    def addmsg(self, msg):
+        self.msgs.append(msg)
+
+    def countit(self):
+        count_types = 'all abs like boo neutral'.split()
+        self.count= dict(zip(count_types,[0, 0, 0, 0, 0]))
+        for msg in self.msgs:
+            if msg.type == '推':
+                self.count['like'] += 1
+            elif msg.type == '噓':
+                self.count['boo'] += 1
+            else:
+                self.count['neutral'] += 1
+
+        self.count['all'] = self.count['like'] + self.count['boo'] + self.count['neutral']
+        self.count['score'] = self.count['like'] - self.count['boo']
+
+    @property
+    def simple_expression(self):
+        msgs = []
+        attrs = ['type', 'user', 'content', 'ipdatetime']
+        for msg in self.msgs:
+            msgs.append(dict(zip(attrs, list(msg))))
+        return msgs
+
+
+# alias
+Summary = ArticleSummary
+Article = ArticlePage
+Board = ArticleListPage.from_board
